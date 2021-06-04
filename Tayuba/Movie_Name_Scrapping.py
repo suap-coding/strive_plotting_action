88c713d752906d73fdc2ad55b876c90edc717d5f
Ayuba Tahiru <ayubamonnietahiru@gmail.com>
2021-06-04 15:44:07 +0200

    explain

--- Tayuba/Movie_Name_Scrapping.py
+++ Tayuba/Movie_Name_Scrapping.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-# website = "https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,asc&start=51&ref_=adv_nxt"
 movies1_50 = "https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,asc"
 movies51_100 = "https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,asc&start=51&ref_=adv_nxt"
 response = requests.get(movies1_50)
@@ -10,6 +9,7 @@ soup = BeautifulSoup(response.content, "html.parser")
 
 # travers through the soup
 names_soup = soup.find_all(name="h3")
+# print(names_soup)
 
 # create a list
 names_list = []
@@ -33,18 +33,8 @@ for idx in range(0, len(names_soup)-1):
 names_series = pd.Series(names_list, name="Movie name")
 print(names_series)
 
+# soup of movie types
+# movie_types_soup = soup.find_all(name="span", class_="genre")
+# for type_movies in movie_types_soup:
+#     type_movies.getText()
 
-# types_of_movies = soup.find_all(name="span", class_="genre")
-
-
-
-
-# for type_of_movie in types_of_movies:
-#     print(type_of_movie.getText())
-
-# df = pd.DataFrame(data)
-# print(df)
-# csv_file = open("data.csv", mode="w+")
-# csv_file.writelines(df)
-
-# df_csv =df.to_csv("data.csv")
