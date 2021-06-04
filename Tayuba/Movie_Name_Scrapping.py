342cce70c3f8c5abbc1369f16f7793929308ff08
Ayuba Tahiru <ayubamonnietahiru@gmail.com>
2021-06-04 16:49:16 +0200

    genre

--- Tayuba/Movie_Name_Scrapping.py
+++ Tayuba/Movie_Name_Scrapping.py
@@ -18,23 +18,30 @@ names_list = []
 for idx in range(0, len(names_soup)-1):
     names_list.append(list(names_soup[idx])[3].text)
 
+
 movies51_100 = "https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,asc&start=51&ref_=adv_nxt"
 response = requests.get(movies51_100)
 soup = BeautifulSoup(response.content, "html.parser")
 
 # travers through the soup
 names_soup = soup.find_all(name="h3")
+# print(names_soup)
 
 # append the next 50
 for idx in range(0, len(names_soup)-1):
     names_list.append(list(names_soup[idx])[3].text)
+# print(names_list)
 
-# make a pd.Series of it
 names_series = pd.Series(names_list, name="Movie name")
-print(names_series)
+# print(names_series)
 
-# soup of movie types
-# movie_types_soup = soup.find_all(name="span", class_="genre")
-# for type_movies in movie_types_soup:
-#     type_movies.getText()
+names_of_genre = []
+
+movie_types_soup = soup.find_all(name="span", class_="genre")
+
+for idx1 in range(0, len(movie_types_soup)- 1):
+    names_of_genre.append(list(movie_types_soup[idx1])[0])
+# print(names_of_genre)
+movie_types_series = pd.Series(names_of_genre, name="Genre")
+print(movie_types_series)
 
